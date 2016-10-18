7db0a2fcd5aa39f419b15faf364e8149e17403d9
ebrun <ebrun@commercialtribe.com>
2016-10-18 15:10:55 -0600

    list all switches first

--- fire-watch.py
+++ fire-watch.py
@@ -22,7 +22,8 @@ slack_api_token = os.environ['SLACK_API_TOKEN']
 
 firestatus = False
 
-print "Starting Slack API check for fire channel!"
+print "Starting Slack API check for fire channel! Swtiches:"
+env.list_switches()
 channels_api_url = 'https://slack.com/api/channels.list?token=%s&pretty=1' % slack_api_token
 
 r = requests.get(channels_api_url)
@@ -31,15 +32,18 @@ if r.status_code != 200:
     exit(1)
 
 # get string channel name from encoded response and compare
-for channel in r.json()['channels']:
-    if 'fire-' in str(channel['name']) and str(channel['is_archived']) != 'True':
-        print "Fire channel found: %s" % channel['name']
-        print "Channel archived status: %s " % channel['is_archived']
-        firestatus = True
-        # Turn on all switches in this lan
-        for switch in (env.list_switches()):
-            print("Turning On : " + switch)
-            env.get_switch(switch).on()
+try:
+    for channel in r.json()['channels']:
+        if 'fire-' in str(channel['name']) and str(channel['is_archived']) != 'True':
+            print "Fire channel found: %s" % channel['name']
+            print "Channel archived status: %s " % channel['is_archived']
+            firestatus = True
+            # Turn on all switches in this lan
+            for switch in (env.list_switches()):
+                print("Turning On : " + switch)
+                env.get_switch(switch).on()
+except KeyError:
+    print "Couldnt get slack channels; is API token env variable correct?"
 
 if not firestatus:
     # Turn off all switches
